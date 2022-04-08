09321716820bc17f13dddef57b2c5595a149767e
Adeetdahal <dahaladit61@gmail.com>
2022-04-08 20:59:03 +0545

    updated

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+
+import { SocialIcons } from '../Header/HeaderStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList style={{display:'flex', 
+    justifyContent:'space-between'}}>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+9779861194747">+977-9861194747</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:dahaladit61@gmail.com">
+            dahaladit61@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating through bits and digits</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/Adeetdahal">
+            <AiFillGithub size="2.8rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/adit-dahal/">
+            <AiFillLinkedin size="2.8rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.instagram.com/aditdahal/">
+            <AiFillInstagram size="2.8rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
---
+++ src/constants/constants.js
@@ -0,0 +1,18 @@
+export const projects = [
+  {
+    title: 'Tax Pay',
+    description: "Full Stack MERN Application rationalizing the Experience of vehicle tax payment and collection service.",
+      image: '/images/1.png',
+      tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://https://github.com/Adeetdahal/TaxPay.com',
+    id: 0,
+  }  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey initiating with Python', },
+  { year: 2018, text: 'Commenced the Bsc. CSIT program', },
+  { year: 2019, text: 'Explored HTML, CSS, JS ', },
+  { year: 2020, text: 'Exhibited th frontend tools, especislly ReactJS', },
+  { year: 2021, text: 'Looking for opportunities', },
+];
