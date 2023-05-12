bcb3fe8b02567b283f1758a60cee9641c0e99718
Adeetdahal <dahaladit61@gmail.com>
2023-05-12 23:32:08 +0545

    WIP: added timeline data of journey

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -18,9 +18,11 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey initiating with Python', },
-  { year: 2018, text: 'Commenced the Bsc. CSIT program', },
-  { year: 2019, text: 'Explored HTML, CSS, JS ', },
-  { year: 2020, text: 'Exhibited th frontend tools, especislly ReactJS', },
-  { year: 2021, text: 'Looking for opportunities', },
+  { year: 2017, text: 'Embarked on my journey with Python', },
+  { year: 2018, text: 'Commenced the B.Sc. CSIT program', },
+  { year: 2019, text: 'Delved into HTML, CSS & JS ', },
+  { year: 2020, text: 'Exhibited the frontend tools, especially ReactJS', },
+  { year: 2021, text: 'Completed college projects with ReactJs', },
+  { year: 2022, text: 'Completed B.Sc. CSIT program and started Internship', },
+  { year: 2023, text: 'Employed as a Front-End Developer @DanpheInnovations', },
 ];
