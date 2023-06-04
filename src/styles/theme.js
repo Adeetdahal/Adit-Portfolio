192e9e63ab5c295572b38cafd54c672001d34c57
Adeetdahal <dahaladit61@gmail.com>
2023-06-04 06:07:50 +0545

    removing tsparticles

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,11 +2,11 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from "../themes/default";
 import GlobalStyles from './globals';
-import ParticleComponent from '../../utils/Particles/ParticleComponent';
+// import ParticleComponent from '../../utils/Particles/ParticleComponent';
 
 const Theme = ({ children }) => (
   <ThemeProvider theme={theme}>
-    <ParticleComponent />
+    {/* <ParticleComponent /> */}
     <GlobalStyles />
     {children}
   </ThemeProvider>
